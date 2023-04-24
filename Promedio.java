10c66089f5a92baaa924c5eb244bb15ea9e8f1c8
Felipe <n.felialvarez@gmail.com>
2023-04-23 23:13:47 -0300

    First commit

---
+++ Promedio.java
@@ -0,0 +1,14 @@
+public class Promedio {
+	public static void main(String arg[]) {
+	
+	int primerNumero = 8;
+	int segundoNumero = 8;
+	int tercerNumero = 9;
+	float promedio = 0.0f;
+		
+
+	promedio = (primerNumero + segundoNumero + tercerNumero) / 3;
+
+	System.out.println("El promedio entre estos numeros es: " + promedio);
+}
+}
---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables{
+	public static void main(String args[]){
+	// comentario de una linea
+	/* comentario de
+	dos lineas.
+	*/
+	int primerNumero;
+	int segundoNumero = 5;
+	int resultado;
+
+	primerNumero = 6;
+	
+	resultado = primerNumero + segundoNumero;
+	
+	System.out.println("El resultado es: " + resultado);
+}
+}
