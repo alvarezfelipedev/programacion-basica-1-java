a0d50b9d151ecde595524f3a4bee1712dca55112
Felipe <n.felialvarez@gmail.com>
2023-05-15 22:17:35 -0300

    Update EjemploDeAscensor.java

--- TP 3°/InterfazConElUsuario/ar/edu/unlam/pb1/dominio/EjemploDeAscensor.java
+++ TP 3°/InterfazConElUsuario/ar/edu/unlam/pb1/dominio/EjemploDeAscensor.java
@@ -1,10 +1,24 @@
 package ar.edu.unlam.pb1.dominio;
 
+import ar.edu.unlam.pb1.ManejoAscensor;
+import ar.edu.unlam.pb1.Persona;
+
 public class EjemploDeAscensor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ManejoAscensor consorcioCallePeron = new ManejoAscensor(4,300);
+		System.out.println("El piso actual es: " + consorcioCallePeron.getPisoActual());
+		consorcioCallePeron.subir();
+		consorcioCallePeron.subir();
+		consorcioCallePeron.subir();
+		System.out.println("El piso actual es: " + consorcioCallePeron.getPisoActual());
+		
+		Persona lucas = new Persona("Lucas","Masculino", 43321521,60,170);
+		Persona maria = new Persona("Maria","Femenino", 43325251,80,165);
+		Persona marcos = new Persona("Marcos","Masculino", 43321521,90,180);
+		
+		consorcioCallePeron.subir();
+		
 	}
 
 }
